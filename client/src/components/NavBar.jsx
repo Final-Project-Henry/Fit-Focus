c5a4c8c7950f488ea5ce1943d93334d40078f479
RafaelBetancourt <rafael18282@gmail.com>
2022-09-08 09:54:13 -0500

    un lorem para navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ function NavBar() {
   return (
     <div>
         <h1>Aqui va la Navbar.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ad quo laudantium excepturi! Mollitia, a repudiandae nemo pariatur rerum recusandae minima quisquam ipsum accusantium obcaecati voluptas, quis ipsam laboriosam reprehenderit.</p>
     </div>
   )
 }
