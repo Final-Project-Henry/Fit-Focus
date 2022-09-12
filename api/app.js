e7bae77715e631f2a190bd59c1563b7b83f0b0dd
x1vaan <ivanx467@gmail.com>
2022-09-12 20:21:41 -0300

    install mongo-connect

--- api/app.js
+++ api/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const freeAccess = require('./routes/freeAccess.js');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('passport-local').Strategy;
+
+
 require('dotenv').config();
 
+const {SECRET} = process.env
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
 app.use((req, res, next) => {  
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Credentials', 'true');
@@ -14,6 +22,18 @@ app.use((req, res, next) => {
     next();
   });
 
+  app.use(session({
+    secret: SECRET,
+    resave: true,
+    saveUninitialized: true,
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session())
+
   app.use('/', freeAccess); //Rutas que puede acceder sin estar registrado/logeado
+  
+ 
+
 
   module.exports = app
