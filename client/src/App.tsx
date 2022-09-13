464a5e840d8668bb03ed2cf627535af00ab71de1
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-13 20:53:16 -0300

    feat: mejoras en el registro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-
 import { Route } from "react-router-dom";
-
-import logo from './logo.svg';
-import { Route } from "react-router-dom";
-import HomeVisitor from './Components/HomeVisitor/HomeVisitor';
-
-import './App.css';
+import HomeVisitor from './components/HomeVisitor/HomeVisitor';
 import SingUp from './components/login-singUp/sing-up';
 
 function App() {
