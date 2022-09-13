f3397de9a3d4ccf2179c12034f16fb871f0f75e2
x1vaan <ivanx467@gmail.com>
2022-09-13 16:49:21 -0300

    fix

--- api/app.js
+++ api/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const freeAccess = require('./routes/freeAccess.js');
 const passport = require('passport');
 const passportLocal = require('passport-local').Strategy;
+
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 
 const user = require('./models/User.js')
 
 const MongoStore = require('connect-mongo');  // Para guardar datos de la sesion en mongo
-const User = require('./models/User.js');
+
 
 require('dotenv').config();
 const {SECRET, MONGODB_URI} = process.env
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   });
+
 
 app.use(session({ // creando una sesion
   secret: SECRET,
@@ -69,6 +71,8 @@ passport.deserializeUser((userid, done) => {
 })
 
 
-  app.use('/', freeAccess); //Rutas que puede acceder sin estar registrado/logeado
+  
+app.use('/', freeAccess); //Rutas que puede acceder sin estar registrado/logeado
 
   module.exports = app
+  
--- api/routes/freeAccess.js
+++ api/routes/freeAccess.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const user = require('../models/User.js');
 const passport = require('passport');
 
+
 const router = Router();
 
 router.post('/register', async (req, res) => {
@@ -15,10 +16,12 @@ router.post('/register', async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 10);
 
     const User = await user.create({name,email,password : hashPassword});
+
     res.status(201).send(User);
   } catch (error) {
     res.status(500).send(error.message);
   }
+
 });
 
 router.post('/login', passport.authenticate('local',{failureRedirect: '/'}), (req, res) => {
@@ -36,6 +39,7 @@ router.get('/getexercises', async (req,res) => {
 
 router.get('/', (req, res) => {
 res.status(200).send('fallaste')
+
 });
 
 module.exports = router
