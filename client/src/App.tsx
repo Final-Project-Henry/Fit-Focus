a498d8045c70874d54fb98296dc514bb4f7e5a36
David <david.cicconi94@gmail.com>
2022-09-14 18:26:07 -0300

    add navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,25 @@
-
-
-import React from 'react';
+import React from "react";
 import { Route } from "react-router-dom";
-import { LandingPage } from './components/LandingPage/LandingPage';
-import HomeVisitor from './components/HomeVisitor/HomeVisitor';
-import Navbar from "./Components/Navbar/Navbar";
-import Login from './components/login-singUp/Login'; 
-import SingUp from './components/login-singUp/sing-up';
-
+import { LandingPage } from "./components/LandingPage/LandingPage";
+import HomeVisitor from "./components/HomeVisitor/HomeVisitor";
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/login-singUp/Login";
+import SingUp from "./components/login-singUp/sing-up";
 
 function App() {
   return (
     <React.Fragment>
-
       <Navbar />
-     
-      <Route path="/" component={LandingPage } />
- 
+
+      <Route path="/" component={LandingPage} />
+
       <Route path="/home" component={HomeVisitor} />
 
-    <Route exact path="/singUp" component={SingUp} />
-    
-    <Route exact path="/login" component={Login} />
-    <Route path ="/home" component={HomeVisitor}/>
+      <Route exact path="/singUp" component={SingUp} />
 
-  
-
+      <Route exact path="/login" component={Login} />
+      <Route path="/home" component={HomeVisitor} />
     </React.Fragment>
-
   );
 }
 
