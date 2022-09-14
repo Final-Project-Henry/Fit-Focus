62e05a17a2bb8918a8d1f65414d4ec2190be6161
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-14 17:18:16 -0300

    fix:arreglo de conflicto

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,24 @@
-<<<<<<< HEAD
-//import React from 'react';
-//import logo from './logo.svg';
-//import { Counter } from './features/counter/Counter';
-import './App.css';
-import { LandingPage } from './components/LandingPage/LandingPage';
-
-function App() {
-  return (
-    <LandingPage />
-=======
 import React from 'react';
 import { Route } from "react-router-dom";
-import HomeVisitor from './components/HomeVisitor/HomeVisitor';
-import Login from './components/login-singUp/Login'; 
+import HomeVisitor from './Components/HomeVisitor/HomeVisitor';
+import Login from './Components/login-singUp/Login'; 
+import { LandingPage } from './Components/LandingPage/LandingPage';
 
-import SingUp from './components/login-singUp/sing-up';
+
+import SingUp from './Components/login-singUp/sing-up';
 
 
 function App() {
   return (
     <React.Fragment>
-
+    <Route exact path="/" component={LandingPage} />
     <Route exact path="/singUp" component={SingUp} />
-    
     <Route exact path="/login" component={Login} />
     <Route path ="/home" component={HomeVisitor}/>
-
+    
   
     </React.Fragment>
->>>>>>> 1992478d84ea4524e0c77437a5ff6df03789acb9
+
   );
 }
 
