1b4aac7f0e34eda96b458129d3d71c1bf70a049a
Rafael Betancourt <Rafael18282@gmail.com>
2022-09-14 16:41:10 -0500

    Landing fixed (#45)
    * Landing Page
    
    * app fixed route
    
    * Titulo agrandado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
+
 import React from 'react';
 import { Route } from "react-router-dom";
-import HomeVisitor from './Components/HomeVisitor/HomeVisitor';
-import Login from './Components/login-singUp/Login'; 
-import { LandingPage } from './Components/LandingPage/LandingPage';
 
-
-import SingUp from './Components/login-singUp/sing-up';
+import HomeVisitor from './components/HomeVisitor/HomeVisitor';
+import Login from './components/login-singUp/Login'; 
+import LandingPage  from './components/LandingPage/LandingPage';
+import SingUp from './components/login-singUp/sing-up';
 
 
 function App() {
   return (
     <React.Fragment>
+
     <Route exact path="/" component={LandingPage} />
     <Route exact path="/singUp" component={SingUp} />
     <Route exact path="/login" component={Login} />
     <Route path ="/home" component={HomeVisitor}/>
-    
-  
+
     </React.Fragment>
 
   );
