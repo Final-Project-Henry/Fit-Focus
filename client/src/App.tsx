ff056f549e3b28a78684e452cd13ba1ab3d60e3b
David <david.cicconi94@gmail.com>
2022-09-14 12:54:51 -0300

    add dropdown

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Route } from "react-router-dom";
-import HomeVisitor from './components/HomeVisitor/HomeVisitor';
-import SingUp from './components/login-singUp/sing-up';
+import Navbar from "./Components/Navbar/Navbar";
+/* import HomeVisitor from './components/HomeVisitor/HomeVisitor';
+import SingUp from './components/login-singUp/sing-up'; */
 
 function App() {
   return (
     <React.Fragment>
-
-    <Route exact path="/login" component={SingUp} />
-    <Route path ="/home" component={HomeVisitor}/>
-  
+      <Navbar />
+      {/* <Route exact path="/login" component={SingUp} />
+    <Route path ="/home" component={HomeVisitor}/> */}
     </React.Fragment>
   );
 }
 
 export default App;
-
---
+++ client/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+// Navbar.tsx
+import { Link, useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(true);
+
+  return (
+    <div>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Fit-Focus
+            </span>
+          </a>
+          <div className="flex items-center md:order-2">
+            <button
+              type="button"
+              className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+              id="user-menu-button"
+              aria-expanded="false"
+              data-dropdown-toggle="user-dropdown"
+              data-dropdown-placement="bottom"
+            >
+              <span className="sr-only">Open user menu</span>
+              <img
+                onClick={() => setShowMenu(!showMenu)}
+                className="w-8 h-8 rounded-full"
+                src="/docs/images/people/profile-picture-3.jpg"
+                alt="user photo"
+              />
+            </button>
+            {/* Dropdown menu */}
+            {showMenu && (
+              <div
+                className=" z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                id="user-dropdown"
+                data-popper-reference-hidden
+                data-popper-escaped
+                data-popper-placement="bottom"
+                style={{
+                  position: "absolute",
+                  marginTop: "320px",
+                  padding: "10px",
+                }}
+              >
+                <div className="py-3 px-4">
+                  <span className="block text-sm text-gray-900 dark:text-white">
+                    User Nameasdasdasd
+                  </span>
+                  <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                    david.cicconi94@gmail.com
+                  </span>
+                </div>
+                <ul className="py-1" aria-labelledby="user-menu-button">
+                  <li>
+                    <Link
+                      to="/profile"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      My Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/settings"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Settings
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/dashboard"
+                      href="#"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/"
+                      href="#"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Sign out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  to="/home"
+                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to=""
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/feedbacks"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Feedbacks
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/products"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
