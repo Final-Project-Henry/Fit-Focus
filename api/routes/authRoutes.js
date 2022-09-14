d05a275078d55152aa2295cd00e785223443d897
x1vaan <ivanx467@gmail.com>
2022-09-14 19:04:42 -0300

    .

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-
+const user = require('../models/User.js')
 const router = Router();
 
-router.get('/getexercises', (req,res) => {
-console.log(req.user)
-console.log(req.user.name)
-res.status(200).send('llegaste')
+router.get('/getexercises', async (req,res) => {
+   const {email} = req.user
+   const User = await user.findOne({email : email}).select('plan')
+   
+res.status(200).send(User)
 });
 
 module.exports = router
