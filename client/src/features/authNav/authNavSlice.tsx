fe0de85bb1f30dc5aa4c018184aa74fa923a0869
David Cicconi <57296669+davidcicconi94@users.noreply.github.com>
2022-09-14 18:43:46 -0300

    navbar (#44)
    * add dropdown
    
    * add navbar
    
    * add navbar component
    
    * resolving conflicts
    
    Co-authored-by: Marcel <98562281+LautaroFranc@users.noreply.github.com>
    Co-authored-by: Caterina Aracil <caterinaaracil@gmail.com>

---
+++ client/src/features/authNav/authNavSlice.tsx
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+  isLogged: boolean;
+}
+
+const initialState: AuthState = {
+  isLogged: true,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction) => {
+      state.isLogged = true;
+    },
+    logout: (state, action: PayloadAction) => {
+      state.isLogged = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
