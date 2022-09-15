c976318cdb34c520f461b0b94ab15840db66a936
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-09-15 08:52:14 -0400

    Arreglo de rutas y actualizacion de Router v5 to Router v6 (#52)
    * Guardando mi trabajo para hacer pull
    
    * Prueba
    
    * Arreglo y actualizacion de rutas, de V5 a V6.

---
+++ client/src/Components/HomeRegister/HomeRegister.tsx
@@ -0,0 +1,13 @@
+import "./styles/HomeRegister.css"
+import Carousel from "./Carousel"
+
+const HomeRegister = () => {
+
+    return (
+        <div className="max-w-screen-lg my-12 mx-auto overflow-hidden">
+            <Carousel />
+        </div>
+    )
+}
+
+export default HomeRegister
