65838f8e1cea48946b470befd07400033e540b86
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-09-14 20:52:28 -0400

    Prueba

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import { LandingPage } from "./components/LandingPage/LandingPage";
-import HomeVisitor from "./components/HomeVisitor/HomeVisitor";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./components/login-singUp/Login";
-import SingUp from "./components/login-singUp/sing-up";
+import { Route,useLocation } from "react-router-dom";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import HomeVisitor from "./Components/HomeVisitor/HomeVisitor";
+import HomeRegister from "./Components/HomeRegister/HomeRegister";
+import Navbar from "./Components/Navbar/Navbar";
+import Login from "./Components/login-singUp/Login";
+import SingUp from "./Components/login-singUp/sing-up";
 
 
 
 function App() {
 
+  const {pathname} = useLocation()
+
   return (
     <React.Fragment>
-      <Navbar />
-      <Route exact path="/landing" component={LandingPage} />
-      
+      {pathname!=="/"&&<Navbar/>}
+
+      <Route exact path="/" component={LandingPage} />
       <Route exact path="/singUp" component={SingUp} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/home" component={HomeVisitor} />
+      <Route exact path="/homeRegister" component={HomeRegister}/>
 
     </React.Fragment>
-
   );
 }
 
