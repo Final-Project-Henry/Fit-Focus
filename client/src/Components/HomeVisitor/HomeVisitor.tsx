ae42c513b7f25dfad2e6c476e2431ace682c0d48
David Cicconi <57296669+davidcicconi94@users.noreply.github.com>
2022-09-16 18:09:49 -0300

    add scroll (#85)

--- client/src/Components/HomeVisitor/HomeVisitor.tsx
+++ client/src/Components/HomeVisitor/HomeVisitor.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Carousel from "../Carousel/Carousel";
-import { Div_cards, Div_video,Div_card_skill } from "./styles/styled_componet_homeVisite";
+import {
+  Div_cards,
+  Div_video,
+  Div_card_skill,
+} from "./styles/styled_componet_homeVisite";
 import img1 from "../assets/homeRegister-media/Img1.jpg";
 import img2 from "../assets/homeRegister-media/Img2.jpg";
 import img3 from "../assets/homeRegister-media/Img3.jpg";
@@ -24,8 +28,8 @@ export default function HomeVisitor() {
         <Carousel
           content={[
             { src: img1, text: "hombre fuerte" },
-            { src: img2, text: 'Hombre fuerte 2' },
-            { src: img3, text: "Hombre Fuerte 3" }
+            { src: img2, text: "Hombre fuerte 2" },
+            { src: img3, text: "Hombre Fuerte 3" },
           ]}
         />
       </div>
@@ -33,50 +37,51 @@ export default function HomeVisitor() {
       <Div_cards className="flex  ">
         <div id="card" className="   text-white bg-gray-900">
           <div id="img_card">
-          <img src={foto1} className="foto1" />
+            <img src={foto1} className="foto1" alt="foto" />
           </div>
           <p>
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id dicta magni, deleniti
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id
+            dicta magni, deleniti
           </p>
         </div>
         <div id="card" className=" text-white bg-gray-900">
           <div id="img_card">
-          <img src={foto2} className="foto1" />
+            <img src={foto2} className="foto1" alt="foto" />
           </div>
           <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id dicta magni, deleniti
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id
+            dicta magni, deleniti
           </p>
         </div>
         <div id="card" className=" text-white bg-gray-900">
           <div id="img_card">
-            <img src={foto3} className="foto1" />
+            <img src={foto3} className="foto1" alt="foto" />
           </div>
           <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id dicta magni, deleniti
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo id
+            dicta magni, deleniti
           </p>
         </div>
       </Div_cards>
       <Div_video>
-        <p className="text-white">
-            EL ÚNICO LÍMITE ERES TÚ
-        </p>
-        <video id="video"  src={video} autoPlay loop muted  />
+        <p className="text-white">EL ÚNICO LÍMITE ERES TÚ</p>
+        <video id="video" src={video} autoPlay loop muted />
         {/* <div id="btm_video" className="w-1/4  bg-blue-700 absolute text-white text-center p-2 my-5">
           <button >Empieza a ENTRENAR</button>
         </div> */}
       </Div_video>
-{/* 
+      {/* 
       <div className="FrasesMotivacionales">
         <h1 className="image"></h1>
       </div> */}
 
       <Div_card_skill>
-        <img src={skille}/>
+        <img src={skille} alt="skills" />
       </Div_card_skill>
-      <div className="flex justify-center">
+      <div className="flex justify-center" id="about">
         <p id="abut">Acerca de nosotros</p>
       </div>
-      <div >
+      <div>
         <p className="p-5">
           Somos un grupo de jóvenes y entusiastas programadores que
           desarrollamos aplicaciones web responsive. Nuestra meta es desarrollar
@@ -85,12 +90,9 @@ export default function HomeVisitor() {
         </p>
       </div>
 
-
       <footer className="bg-black">
         <div className="text-white">
-          <p >
-             PROYECTO FINAL SOY HENRY 2022
-          </p>
+          <p>PROYECTO FINAL SOY HENRY 2022</p>
         </div>
       </footer>
     </div>
