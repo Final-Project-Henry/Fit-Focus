b850b98c5d074aa668eb6058172d51b7020ade63
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-17 01:36:36 -0300

    fix y add funcionalidade (#90)
    grasa

--- client/src/Components/login-singUp/Login.tsx
+++ client/src/Components/login-singUp/Login.tsx
@@ -1,78 +1,135 @@
-import React,{ useEffect, useRef,useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import jwtDecode from "jwt-decode";
 
-import { useAppDispatch, useAppSelector  } from "../../app/hooks";
 import {
   User_Login_State,
-
-  selectUser
-} from '../../features/counter/counterSlice';
+  selectUser,
+} from "../../features/counter/counterSlice";
 import facebook from "../assets/login-singup_media/icons8-facebook.svg";
 import google from "../assets/login-singup_media/icons8-google.svg";
 import linkedin from "../assets/login-singup_media/icons8-linkedin-circled.svg";
-export default function Login(){
+export default function Login() {
   let user = useAppSelector(selectUser);
-  const dispatch = useAppDispatch()
-  const [Form_data, Set_form_data] = useState(
-    {
-      email:"",
-      password:"",
+  const dispatch = useAppDispatch();
+  const [Form_data, Set_form_data] = useState({
+    email: "",
+    password: "",
+  });
+  useEffect(() => {
+    console.log("entra", user);
+    if (typeof user.user === "string") {
+      window.localStorage.setItem(
+        "Login_userFit_Focus",
+        JSON.stringify(user.user)
+      );
     }
-  );
-  function handleChange(event:React.ChangeEvent<HTMLFormElement | HTMLInputElement>):void {
-    Set_form_data(pv=>({...pv, [event.target.name] : event.target.value }))
+  }, [user.user]);
 
+  function handleChange(
+    event: React.ChangeEvent<HTMLFormElement | HTMLInputElement>
+  ): void {
+    Set_form_data((pv) => ({ ...pv, [event.target.name]: event.target.value }));
   }
 
-   //////////enviar de datos  por medio de los input//////////////////////////////////////////
-   function handleSubmit(event:React.FormEvent):void {
+  //////////enviar de datos  por medio de los input//////////////////////////////////////////
+  function handleSubmit(event: React.FormEvent): void {
     event.preventDefault();
-    dispatch(User_Login_State(Form_data))
+    dispatch(User_Login_State(Form_data));
   }
-  
-    return(
-    <>
-       <div>{user&&<Navigate to="/HomeRegister" />}</div>
-      <form className="bg-white w-3/4 rounded-2xl p-11" onSubmit={handleSubmit}>
 
-            <div>
+  return (
+    <>
+      <div>{typeof user.user==="string"&&<Navigate to="/HomeRegister" />}</div>
+  
+      <form className="bg-white w-3/4 rounded-2xl p-11" onSubmit={handleSubmit}>
+        <div className="flex-1">
+          <div>
             <label>Email</label>
-              <input type="email"
-                  name="email"
-                  className="border-none w-full "
-                  autoComplete="off"
-                  placeholder="Alex@gmail.com"
-                  value={Form_data.email}
-                  onChange={(event) => handleChange(event)}
-              />
-            </div>
-            <div>
-              <label>contraseña</label>
-              <input type="password" 
-                  name="password"
-                  className="border-none w-full"
-                  autoComplete="off"
-                  placeholder="*******"
-                  value={Form_data.password}
-                  onChange={(event) => handleChange(event)}
-              />
-            </div>
-            <div className="w-full bg-blue-700 my-16  text-white text-center p-2  ">
-              <button type="submit" >iniciar sesión</button>
-            </div>
-            <div id="auth" className="flex ">
-              <div className="rounded p-3">
-                <img src={google}/>
-              </div>
-              <div className="rounded p-3">
-                <img src={facebook} />
-              </div>
-              <div className="rounded p-3">
-                <img src={linkedin}/>
-              </div>
-            </div>
-        </form>
+            <input
+              type="email"
+              name="email"
+              className="border-none w-full "
+              autoComplete="off"
+              placeholder="Alex@gmail.com"
+              value={Form_data.email}
+              onChange={(event) => handleChange(event)}
+            />
+          </div>
+          <div className="my-5">
+            <label>contraseña</label>
+            <input
+              type="password"
+              name="password"
+              className="border-none w-full"
+              autoComplete="off"
+              placeholder="*******"
+              value={Form_data.password}
+              onChange={(event) => handleChange(event)}
+            />
+          </div>
+        </div>
+        <div className="flex items-start my-3">
+          <div className="flex items-center h-5 ">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+              required
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Remember me
+          </label>
+        </div>
+        <div
+          id="btm_submit"
+          className="w-full bg-blue-700   text-white text-center  "
+        >
+          <button
+            className="w-full bg-blue-700   text-white text-center p-2 "
+            type="submit"
+          >
+            {!user.status?"iniciar sesión":
+              "Loading..."
+            }
+          </button>
+        </div>
+        <div className="flex items-start">
+          <a
+            href="#"
+            className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+          >
+            Lost Password?
+          </a>
+        </div>
+
+        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+          Not registered?{" "}
+          <a
+            href="#"
+            className="text-blue-700 hover:underline dark:text-blue-500"
+          >
+            Create account
+          </a>
+        </div>
+      </form>
+      <div id="auth" className="flex ">
+        <div className="rounded p-3">
+          <img src={google} />
+        </div>
+        <div className="rounded p-3">
+          <img src={facebook} />
+        </div>
+        <div className="rounded p-3">
+          <img src={linkedin} />
+        </div>
+      </div>
     </>
-      
-    )
+  );
 }
--- client/src/components/login-singUp/sing-up.tsx
+++ client/src/components/login-singUp/sing-up.tsx
@@ -27,48 +27,14 @@ export default function SingUp() {
   const [user, setuser] = useState<boolean|object>(false);
 
   const [data_user, set_data_user] = useState<payload | undefined>();
-  const [Auth_google, Set_Auth_Google] = useState<any>();
+
   const [Form_data, Set_form_data] = useState({
     name: "",
     email: "",
     password: "",
   });
 
-  
-  //////////recolectar data del user con goole//////////////////////////////////////////
-  const onGoogleSignIn = async (user: any) => {
-    let userCred = user.credential;
-    set_data_user(jwt_deocde(userCred));
-  };
-  //////////enviar al stado global la data del user obtenido por google//////////////////////////////////////////
-  useEffect(() => {
-    const data = {
-      email: data_user?.email,
-      name: data_user?.name,
-      photo: data_user?.picture,
-    };
-    if (data_user) {
-      dispatch(User_Register_State(data))
-    }
-  }, [data_user]);
-  //////////actializacion del windo para obtner la api de google//////////////////////////////////////////
-  useEffect(() => {
-    Set_Auth_Google(window);
-  }, [window]);
-  //////////hooks de google y su llamado//////////////////////////////////////////
 
-  useScript("https://accounts.google.com/gsi/client", (): void => {
-    Auth_google &&
-      Auth_google.google.accounts.id.initialize({
-        client_id: clientId,
-        callback: onGoogleSignIn,
-        auto_select: false,
-      });
-    Auth_google &&
-      Auth_google.google.accounts.id.renderButton(googlebuttonref.current, {
-        size: "medium",
-      });
-  });
   //////////obtencion de datos  por medio de los input//////////////////////////////////////////
 
   function handleChange(
@@ -88,49 +54,51 @@ export default function SingUp() {
   console.log(user);
   return (
 
-      // <div className="w-1/2">
-      //   <div>{!user&&<div ref={googlebuttonref}></div>}</div>
-      //   
+   
         <>
-        <div>{user_logeao.user&&<Navigate to="/HomeRegister" />}</div>
-           <form className="bg-white w-3/4 rounded-2xl p-11" onSubmit={handleSubmit}>
-
-          <div>
-            <label>Nombre</label>
-            <input type="text" 
-            name="name"
-            className="w-full px-1"
-            autoComplete="off"
-            placeholder="Alex"
-            value={Form_data.name}
-            onChange={(event) => handleChange(event)}
-            />
-          </div>
-          <div className="my-5">
-          <label>Email</label>
-            <input type="email" 
-              name="email"
+        {user_logeao.user&&<Navigate to="/auth/login"/>}
+          <form className="bg-white w-3/4 rounded-2xl p-11" onSubmit={handleSubmit}>
+          <div className="flex-1">
+            <div>
+              <label>Nombre</label>
+              <input type="text" 
+              name="name"
               className="w-full px-1"
               autoComplete="off"
-              placeholder="Alex@gmail.com"
-              value={Form_data.email}
-              onChange={(event) => handleChange(event)
-              }/>
-          </div>
-          <div className="my-5">
-            <label>Contraseña</label>
-            <input type="password"
-              name="password"
-              autoComplete="off"
-              className="w-full px-1"
-              placeholder="***********"
-              value={Form_data.password}
+              placeholder="Alex"
+              value={Form_data.name}
               onChange={(event) => handleChange(event)}
-            />
-          </div>
-          <div className="w-full bg-blue-700 my-16  text-white text-center p-2  ">
-              <button type="submit" >Registrarse</button>
+              />
             </div>
+            <div className="my-5">
+            <label>Email</label>
+              <input type="email" 
+                name="email"
+                className="w-full px-1"
+                autoComplete="off"
+                placeholder="Alex@gmail.com"
+                value={Form_data.email}
+                onChange={(event) => handleChange(event)
+                }/>
+            </div>
+            <div className="my-5">
+              <label>Contraseña</label>
+              <input type="password"
+                name="password"
+                autoComplete="off"
+                className="w-full px-1"
+                placeholder="***********"
+                value={Form_data.password}
+                onChange={(event) => handleChange(event)}
+              />
+              </div>
+            </div>
+
+            <div className="w-full bg-blue-700   text-white text-center">
+              <button className="w-full bg-blue-700   text-white text-center p-2 " type="submit" >Registrarse</button>
+            </div>
+          </form>
+
             <div id="auth" className="flex ">
               <div className="rounded p-3">
                 <img src={google}/>
@@ -142,7 +110,6 @@ export default function SingUp() {
                 <img src={linkedin}/>
               </div>
             </div>
-          </form>
       </>
 
   );
