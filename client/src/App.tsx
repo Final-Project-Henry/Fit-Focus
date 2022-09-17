c9696c4e9402720d55f1b10ee58dfcc6062e34ee
David <david.cicconi94@gmail.com>
2022-09-17 12:12:54 -0300

    add change render home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import HomeVisitor from "./Components/HomeVisitor/HomeVisitor";
 import HomeRegister from "./Components/HomeRegister/HomeRegister";
 import Navbar from "./Components/Navbar/Navbar";
+import { useSesion } from "./app/hooks";
 
 import Form_rutinas from "./Components/form_rutinas/From_rutina";
 
@@ -14,14 +15,19 @@ import SingUp_Login from "./Components/login-singUp/Singup_Login";
 function App() {
   const { pathname } = useLocation();
 
+  const user = useSesion();
+
   return (
     <React.Fragment>
       {pathname !== "/" && <Navbar />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth/:id" element={<SingUp_Login />} />
-        <Route path="/home" element={<HomeVisitor />} />
-        <Route path="/homeRegister" element={<HomeRegister />} />
+        {user ? (
+          <Route path="/home" element={<HomeRegister />} />
+        ) : (
+          <Route path="/home" element={<HomeVisitor />} />
+        )}
         <Route path="/feedbacks" element={<FormFeedback />} />
         <Route path="/rutinas" element={<Form_rutinas />} />
       </Routes>
