38854e75ec32267c855398a4343c720e5cbbae4c
x1vaan <ivanx467@gmail.com>
2022-09-17 14:10:29 -0300

    ruta redirect google get access token and id token

--- api/routes/authGoogleRoutes.js
+++ api/routes/authGoogleRoutes.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
+const getGoogleToken = require('../service/userService.js')
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+ const {code} = req.query
+
+ const { id_token, access_token } = await getGoogleToken({code});
+ const url = 'https://www.googleapis.com/oauth2/v2/userinfo'
+ 
+
 res.send('Autenticado con Google!')
 })
 
---
+++ api/service/userInfo.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const axios = require('axios');
+const querystring = require('node:querystring')
+
+module.exports = async function getUserInfo() {
+    
+}
