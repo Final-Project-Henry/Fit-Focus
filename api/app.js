208dfb60b594b1968f3123d03e45d3437926323f
Martin Angulo Gasco <martinangulo1194@gmail.com>
2022-09-18 16:51:19 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
-app.use(cors({origin: 'http://localhost:3002'||'http://localhost:3001'||'http://localhost:3000'}))
+app.use(cors({origin: 'http://localhost:3000'||'http://localhost:3001'||'http://localhost:3000'}))
 app.use((req, res, next) => {  
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Credentials', 'true');
