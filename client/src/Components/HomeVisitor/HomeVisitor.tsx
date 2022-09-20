5fb17b9a354966bd3210a9787315840542747795
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-19 22:16:36 -0300

    img

--- client/src/Components/HomeVisitor/HomeVisitor.tsx
+++ client/src/Components/HomeVisitor/HomeVisitor.tsx
@@ -9,28 +9,38 @@ import {
   Div_about,
 } from "./styles/styled_componet_homeVisite";
 import Footer from "../footer/Footer";
-const img2 = require("../assets/Fotos y Videos HomeVisitor/imgSlide2.2.jpeg");
-const img3 = require("../assets/Fotos y Videos HomeVisitor/imgSlide3.jpg");
-const img4 = require("../assets/Fotos y Videos HomeVisitor/imgSlide4.jpeg");
-const img5 = require("../assets/Fotos y Videos HomeVisitor/imgSlide5.jpeg");
-
+import img6 = require("../assets/Fotos y Videos HomeVisitor/urban-913.png")
 export default function HomeVisitor() {
-  const [data, setData] = useState<any[]>();
 
-  const handleChange = () => {};
-
+  const [data, setData] = useState<string>();
+  function words(){
+    let words=["en casa", "comodo", "y satifecho"];
+    setTimeout(() =>{
+      
+      setData(words[])
+    },2000)
+  }
   return (
     <>
     {/* screen 1  */}
       <div className="h-screen">
-
+        <div>
+          <div>
+            <h1>lorem</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quas et saepe cum earum? Nesciunt est mollitia distinctio laborum nam eos expedita quae iusto minus, natus ea. Quod, iusto odio. </p>
+            <p>Entrena <span>{}</span> </p>
+          </div> 
+          <div>
+          
+          </div> 
+        </div>
+         
       </div>
-   {/* screen 2  */}
-
+   {/* screen 2  rafa */ }
       <div className="h-screen">
 
       </div>
-    {/* screen 3 */}
+    {/* screen 3  */}
       <div className="h-screen">
 
       </div>
@@ -41,7 +51,7 @@ export default function HomeVisitor() {
    {/* screen 5 */}
       <div className="h-screen">
 
-        </div>
+      </div>
       <Footer />
     </>
   );
