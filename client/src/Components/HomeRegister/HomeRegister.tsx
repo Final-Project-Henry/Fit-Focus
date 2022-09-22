66bf81a6f6b3911d1f4d4e50196e86f46c2802d0
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-22 11:07:11 -0300

    feat home registre

--- client/src/Components/HomeRegister/HomeRegister.tsx
+++ client/src/Components/HomeRegister/HomeRegister.tsx
@@ -229,6 +229,7 @@ interface ejercicios {
 
 const HomeRegister = () => {
   const dispatch = useAppDispatch();
+  const [cards , Setcards] = useState<ejercicios | []>([])
   useEffect(() => {
     dispatch(Exercises_Get());
   }, []);
@@ -236,7 +237,9 @@ const HomeRegister = () => {
   const selector = useAppSelector(selectUser);
   const ejercicios: Array<ejercicios> | null = selector.exercises;
 
-  const paginado = () => {};
+  const paginado = () => {
+    
+  };
   return (
     <>
       <div className="grid grid-cols-5 sm:grid-cols-11 gap-5 bg-slate-200  shadow-lg">
@@ -284,12 +287,11 @@ const HomeRegister = () => {
                 difficulty,
                 muscles,
                 genre,
-                equipment,
                 premium,
               }) => {
                 return (
                   <>
-                    <div className={`max-w-sm flex flex-col bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 duration-150 cursor-pointer scale-90 hover:scale-95  hover:outline hover:outline-offset-1 ${
+                    <div className={`max-w-sm flex flex-col bg-white rounded-lg shadow-md duration-150 cursor-pointer scale-90 hover:scale-95  hover:outline hover:outline-offset-1 ${
                         premium
                           ? "outline-blue-400"
                           : difficulty == "easy"
@@ -343,7 +345,7 @@ const HomeRegister = () => {
                       <div className="p-4">
                         <a
                           href="#"
-                          className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-[#6c63ff] duration-150 rounded-lg hover:bg-blue-800"
                         >
                           Empezar
                           <svg
