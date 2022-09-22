98dfe61492f4c159f5bbfd0b4cdb6653adb1264b
rodrisantes <104109457+rodrisantes@users.noreply.github.com>
2022-09-22 16:20:42 -0300

    Premium

--- client/src/Components/HomeRegister/FilterExercises.js
+++ client/src/Components/HomeRegister/FilterExercises.js
@@ -3,6 +3,9 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { Exercises_Get, selectUser } from "../../features/counter/counterSlice";
@@ -22,14 +25,16 @@ export default function FilterExercises() {
   
   const dispatch = useAppDispatch();
   const {exercises} = useAppSelector(selectUser);
+  const filterPremium = exercises.filter(e =>  e.premium === true)
+
+
   const [filtrado, setFiltrado] = useState([]);
   const [selected, setSelected] = useState({
     genre: "none",
     muscle: "none",
-    difficulty: "none"
+    difficulty: "none",
+    premium : "false",
   })
-
-  console.log(exercises)
  
 
   useEffect(() => {
@@ -44,7 +49,7 @@ export default function FilterExercises() {
 
 
     setFiltrado( exercises.filter(e => {
-      let {muscle, genre, difficulty} = false;
+      let {muscle, genre, difficulty,} = false;
 
       if(selected.muscle === "none" ) muscle = true
       else muscle = selected.muscle === e.muscles
@@ -72,9 +77,13 @@ export default function FilterExercises() {
      setSelected({...selected, muscle: target.value})
   }
 
+  
  const handleSelectDifficulty = ({target}) => {
       setSelected({...selected, difficulty: target.value})
  }
+ const handlePremium = () => {
+  setFiltrado(filterPremium)
+}
 
   
 
@@ -139,13 +148,90 @@ export default function FilterExercises() {
           Hard
         </option>
       </select>
+      <button onClick={handlePremium}>
+        PREMIUM
+      </button>
       
-     { <div>
-      <button>
-      {filtrado?.map(e => <div>{e.name
-    }</div>)}
-    </button>
-    </div>  }
+     <div>
+     <Swiper
+              slidesPerView={6}
+              spaceBetween={30}
+              slidesPerGroup={6}
+              loop={true}
+              loopFillGroupWithBlank={true}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className="mySwiper flex content-center items-center justify-center"
+            >
+              {filtrado.map(
+                ({
+                  video,
+                  name,
+                  difficulty,
+                  muscles,
+                  genre,
+                  _id,
+                  description,
+                  premium
+                }) => {
+                 
+                 
+                 
+                    return (
+                      <SwiperSlide className="bg-transparent h-auto">
+                        <div
+                          key={_id}
+                          className={` scale-90 rounded overflow-hidden shadow-lg cursor-pointer duration-300 hover:duration-200 hover:scale-95 hover:outline hover:outline-offset-1 ${
+                            difficulty == "easy"
+                              ? "outline-green-400"
+                              : difficulty == "medium"
+                              ? "outline-yellow-400"
+                              : "outline-red-400"
+                          }`}
+                        >
+                          <img className="w-full" src={video} alt={name} />
+                          <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">{name}</div>
+                            <p className="text-gray-700 text-base">
+                              {description}{" "}
+                            </p>
+                          </div>
+                          <div className="px-6 pt-4 pb-2">
+                            <span
+                              className={`inline-block ${
+                                difficulty == "easy"
+                                  ? "bg-green-200"
+                                  : difficulty == "medium"
+                                  ? "bg-yellow-200"
+                                  : "bg-red-200"
+                              } rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}
+                            >
+                              {difficulty}
+                            </span>
+                            <span
+                              className={`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}
+                            >
+                              {muscles}
+                            </span>
+                            <span
+                              className={`inline-block ${
+                                genre === "man" ? "bg-blue-200" : "bg-pink-200"
+                              } rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}
+                            >
+                              {genre}
+                            </span>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                    );
+                }
+              )}
+            </Swiper>
+
+      </div>
 
     </div>
   );
