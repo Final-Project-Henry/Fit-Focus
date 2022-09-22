049d433c225488e2fda4134df3c9edcebb9b4f6d
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-22 10:31:49 -0300

    feat home regisgtre

--- client/src/Components/HomeRegister/HomeRegister.tsx
+++ client/src/Components/HomeRegister/HomeRegister.tsx
@@ -15,6 +15,7 @@ import {
   selectUser,
 } from "../../features/counter/counterSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { Link } from "react-router-dom";
 
 const video1 = require("../assets/homeRegister-media/Video-Slide.mp4");
 const video2 = require("../assets/homeRegister-media/Video2.mp4");
@@ -234,11 +235,20 @@ const HomeRegister = () => {
 
   const selector = useAppSelector(selectUser);
   const ejercicios: Array<ejercicios> | null = selector.exercises;
+
+  const paginado = () => {};
   return (
     <>
       <div className="grid grid-cols-5 sm:grid-cols-11 gap-5 bg-slate-200  shadow-lg">
         {/* filtro */}
-        <div className="bg-[#287acc]  shadow-lg border col-span-1  h-screen"></div>
+        <div className="bg-white rounded-xl  shadow-lg border col-span-1  h-screen">
+          <div className="p-2">
+            <div className="py-2">Ejercicios</div>
+            <div className="py-2">calculadora</div>
+            <div className="py-2">Noticias</div>
+            <div className="py-2">Rutinas</div>
+          </div>
+        </div>
         {/* carousel */}
         <div className="w-full col-span-7 border h-screen ">
           <div className="min-w-[100%] mx-[100px] m-5   h-[250px] ">
@@ -266,7 +276,7 @@ const HomeRegister = () => {
             />
           </div>
           {/* card de ejercicios */}
-          <section className="grid grid-cols-1">
+          <section className="grid grid-cols-3">
             {ejercicios?.map(
               ({
                 video,
@@ -279,13 +289,7 @@ const HomeRegister = () => {
               }) => {
                 return (
                   <>
-                    <div
-                      key={name}
-                      className={` rounded felx flex-row ${
-                        premium ? "bg-slate-100" : "bg-slate-50"
-                      } ${
-                        premium ? "blur-[5px]" : "blur-0"
-                      } overflow-hidden shadow-lg cursor-pointer duration-300 hover:duration-200 scale-90 hover:scale-95  hover:outline hover:outline-offset-1 ${
+                    <div className={`max-w-sm flex flex-col bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 duration-150 cursor-pointer scale-90 hover:scale-95  hover:outline hover:outline-offset-1 ${
                         premium
                           ? "outline-blue-400"
                           : difficulty == "easy"
@@ -293,19 +297,25 @@ const HomeRegister = () => {
                           : difficulty == "medium"
                           ? "outline-yellow-400"
                           : "outline-red-400"
+                      }
+                      ${
+                        premium ? "bg-slate-100" : "bg-slate-50"
+                      } ${
+                        premium ? "blur-[5px]" : "blur-0"
                       }`}
                     >
-                      <div className="w-[100%] h-[100px] overflow-hidden">
-                        <img
-                          className="w-[100%] h-[100px] object-cover"
+                      <div className="h-[160px] overflow-hidden">
+                      <img
+                          className="rounded-t-lg"
                           src={video}
+                          alt=""
                         />
                       </div>
-                      <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">{name}</div>
-                        <p className="text-gray-700 text-base">{""} </p>
-                      </div>
-                      <div className="px-6 pt-4 pb-2">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            {name}
+                      </h5> 
+                       
+                      <div className="p-3">
                         <span
                           className={`inline-block ${
                             difficulty == "easy"
@@ -324,42 +334,58 @@ const HomeRegister = () => {
                         </span>
                         <span
                           className={`inline-block ${
-                            genre === "man" ? "bg-blue-200" : "bg-pink-200"
+                            genre === "man" ? "bg-blue-400" : "bg-pink-300"
                           } rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}
                         >
                           {genre}
                         </span>
                       </div>
-                      <div>
-                        <span>{premium}</span>
+                      <div className="p-4">
+                        <a
+                          href="#"
+                          className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                          Empezar
+                          <svg
+                            aria-hidden="true"
+                            className="ml-2 -mr-1 w-4 h-4"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                              clip-rule="evenodd"
+                            ></path>
+                          </svg>
+                        </a>
                       </div>
                     </div>
-                  </>
+                    </>
                 );
               }
             )}
           </section>
         </div>
 
-        <div className="w-full h-[98vh]  col-span-3  mt-[5px] rounded-xl bg-gray-100">
-          <p className="text-white text-2xl p-2 font-black">
+        <div className="w-full h-[98vh]  col-span-3  mt-[5px] rounded-xl bg-white">
+          <p className="text-gray-700 text-2xl p-5 font-black">
             ejercicios populares
           </p>
           {populare.map((e) => {
             return (
-              <div className="flex bg-white p-4 shadow-2xl mx-[20px] my-[10px] rounded-xl">
-                <div className="w-[20%] h-[50px]  overflow-hidden rounded-2xl">
+              <div className="flex bg-white p-2 mt-4 outline outline-blue-400 shadow-lg mx-[20px] my-[10px] rounded-xl">
+                <div className="w-[20%] h-[50px]  overflow-hidden rounded-xl">
                   <img
-                    className=" w-[100%] min-h-[20px] rounded-2xl"
+                    className=" w-[100%] min-h-[50px] rounded-xl"
                     src={e.video}
                   />
                 </div>
                 <div className="px-5">
                   <p>{e.name} </p>
                   <p>{e.difficulty} </p>
-                  <div className="flex items-center">
-                    
-                  </div>
+                  <div className="flex items-center"></div>
                 </div>
               </div>
             );
@@ -373,8 +399,8 @@ const HomeRegister = () => {
 
 export default HomeRegister;
 
-
-{/*     lo voy a usar                   <svg
+{
+  /*     lo voy a usar                   <svg
                       aria-hidden="true"
                       className="w-5 h-5 text-yellow-400"
                       fill="currentColor"
@@ -383,4 +409,5 @@ export default HomeRegister;
                     >
                       <title>Rating star</title>
                       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg> */}
+                    </svg> */
+}
