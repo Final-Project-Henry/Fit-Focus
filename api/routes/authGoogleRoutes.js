6a699c57a2727fdedf55d538ddcb51ea68132d24
Martin Angulo <martinangulo1194@gmail.com>
2022-09-22 10:29:04 -0500

    back/fix routes

--- api/routes/authGoogleRoutes.js
+++ api/routes/authGoogleRoutes.js
@@ -10,53 +10,47 @@ const nodemailer = require('nodemailer')
 
 require('dotenv').config();
 
-const {SECRET} = process.env
-const {NODEMAILER} = process.env
+const { SECRET, NODEMAILER } = process.env
 
 const router = Router();
 
 router.post('/', async (req, res) => {
-    try {
-        const {code} = req.body
+  try {
+    const { code } = req.body
+    const data = jwt.decode(code);
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'fitfocushenry@gmail.com',
+        pass: NODEMAILER,
+      },
 
-         const data = jwt.decode(code);
-        let oldUser = await user.findOne({email : data.email});
-     if(!oldUser) {
-        const hashPassword = await bcrypt.hash(data.sub, 10)
-        oldUser = await user.create({email :data.email, name : data.name, avatar : data.picture, password : hashPassword});
-      
-        let transporter = nodemailer.createTransport({
-         service: 'gmail',
-         auth: {
-           user: 'fitfocushenry@gmail.com',
-           pass: NODEMAILER,
-         },
-       
-       });
-   
-       let mailDetails = {
-         from: 'fitfocushenry@gmail.com',
-         to: email,
-         subject: '¡Bienvenido a Fit Focus!',
-         html:"<b> ¡Su usuario ha sido creado con éxito! Nos alegra que te hayas unido a esta nueva aventura </b>",
-       };
-   
-       transporter.sendMail(mailDetails, (error, info) => {
-         if (error) {
-           res.status(500).send(error.message);
-         } else {
-           console.log('Email enviado');
-           res.status(200).json(req.body);
-         }
-       });
+    });
 
-     } 
-     const token = jwt.sign({email : oldUser.email, name : oldUser.name, id : oldUser._id}, "" + SECRET)
-      
-     res.status(200).send(token)
-    } catch (error) {
-       res.status(500).send(error.message) 
+    let mailDetails = {
+      from: 'fitfocushenry@gmail.com',
+      to: data.email,
+      subject: '¡Bienvenido a Fit Focus!',
+      html: "<b> ¡Su usuario ha sido creado con éxito! Nos alegra que te hayas unido a esta nueva aventura </b>",
+    };
+    let oldUser = await user.findOne({ email: data.email });
+    if (!oldUser) {
+      const hashPassword = await bcrypt.hash(data.sub, 10)
+      oldUser = await user.create({ email: data.email, name: data.name, avatar: data.picture, password: hashPassword });
+      console.log(data.email)
+
+      transporter.sendMail(mailDetails, (error, info) => {
+        if(error)console.log('error: ',error);
+        else console.log('mensaje enviado con exito');
+      });
+
     }
+    const token = jwt.sign({ email: oldUser.email, name: oldUser.name, id: oldUser._id }, "" + SECRET)
+
+    res.status(200).send(token)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 })
 
 module.exports = router
