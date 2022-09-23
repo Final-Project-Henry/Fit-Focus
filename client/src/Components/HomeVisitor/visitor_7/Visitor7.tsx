09368286c072baa56bec3a9c412910face1465b2
Martin Angulo <martinangulo1194@gmail.com>
2022-09-23 07:17:51 -0500

    front/ fix css to 1366px width

--- client/src/Components/HomeVisitor/visitor_7/Visitor7.tsx
+++ client/src/Components/HomeVisitor/visitor_7/Visitor7.tsx
@@ -13,13 +13,13 @@ interface dev {
 export default function Visitor7() {
 
   return (
-    <div style={{display:'flex', flexDirection:'column', alignItems:"center"}}>
-      <h1 style={{fontSize:"3.5rem", color:"#111828", marginTop:"5vh"}} >Acerca de Nosotros</h1>
-      <div style={{display:'grid', gridTemplateColumns:"1fr 1fr 1fr 1fr", width:"80vw", gap:"50px", margin: "5vh 0 5vh 0"}}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: "center" }}>
+      <h1 style={{ fontSize: "3.5rem", color: "#111828", marginTop: "5vh" }} >Acerca de Nosotros</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: "1fr 1fr 1fr 1fr", width: "90vw", gap: "50px", margin: "5vh 0 5vh 0" }}>
         {
-        devs.map((e:dev)=>(
-          <Dev data={e}/>
-        ))
+          devs.map((e: dev) => (
+            <Dev data={e} />
+          ))
         }
       </div>
     </div>
