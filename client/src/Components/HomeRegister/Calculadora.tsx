6bf0e389d15bc2bd2ad9affd2bc5683522440eff
Rafael Betancourt <Rafael18282@gmail.com>
2022-09-23 19:28:09 -0500

    Calculator (#194)
    * package update
    
    * Calculadora IMC

--- client/src/Components/HomeRegister/Calculadora.tsx
+++ client/src/Components/HomeRegister/Calculadora.tsx
@@ -1,10 +1,107 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import señorasala from "../assets/homeRegister-media/señorasala.png";
+import { useState } from 'react';
+import "./styles/calculadora.css";
 
 
-export default function Calculadora() {
+function Calculadora() {
+
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+
+  const [mensaje, setMensaje] = useState("");
+  const [imcMensaje, setImcMensaje] = useState("");
+
+
+  function calcularIMC(altura: number, peso: number) {
+
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (altura <= 0 || peso <= 0) {
+      alert("Error. Por Favor, agregue el peso y la altura corretamente!");
+      
+    } else if (!alt) {
+      alert("Error. Por Favor, agregue o peso y la altura corretamente!");
+
+    } else if (imc < 16.9) {
+      setMensaje(`Usted está severamente por debajo del promedio de peso, por favor consulte a un médico antes de proseguir!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted está severamente por debajo del promedio de peso, por favor consulte a un médico antes de proseguir!");
+
+    } else if (imc >= 17 && imc < 18.4) {
+      setMensaje(`Usted está por debajo del peso promedio, por favor consulte a su médico antes de proseguir!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted está por debajo del promedio de peso, por favor consulte a un médico antes de proseguir!");
+
+    } else if (imc >= 18.5 && imc < 24.9) {
+      setMensaje(`Usted se encuentra en un peso promedio!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted se encuentra en un peso promedio!");
+
+    } else if (imc >= 25 && imc < 29.9) {
+      setMensaje(`Usted esta ligeramente por encima de su peso!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted esta ligeramente por encima de su peso!");
+
+    } else if (imc >= 30 && imc < 34.9) {
+      setMensaje(`Usted esta con obesidad grado I, por favor consulte a su médico antes de proseguir!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted esta con obesidad grado I, por favor consulte a su médico antes de proseguir!");
+
+    } else if (imc >= 35 && imc < 40) {
+      setMensaje(`Usted esta con obesidad grado II, por favor consulte a su médico antes de proseguir!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted esta con obesidad grado II, por favor consulte a su médico antes de proseguir!");
+
+    } else if (imc >= 40) {
+      setMensaje(`Usted esta con obesidad grado III, por favor consulte a su médico antes de proseguir!`);
+      setImcMensaje(`Su IMC es de: ${imc.toFixed(2)}`);
+      alert("Usted esta con obesidad grado III, por favor consulte a su médico antes de proseguir!");
+    }
+
+    setPeso("");
+    setAltura("");
+  }
+
+  function onReset() {
+    setMensaje("");
+    setImcMensaje("");
+  }
+
   return (
+
+    <div className="app">
+      <div className="area-input">
+        <h1 className='title'>Calculadora de IMC </h1>
+        <span>Vamos a calcular tu indice de masa corporal </span>
+
+        <input
+          type="number"
+          placeholder="Peso en [KG] Ej: 75"
+          value={peso}
+          onChange={(e: any) => setPeso(e.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Altura en [CM] Ej: 170"
+          value={altura}
+          onChange={(e: any) => setAltura(e.target.value)}
+        />
+
+        <button onClick={() => calcularIMC(Number(altura), Number(peso))}>
+          Calcular
+        </button>
+
+        <h2 className='mensajes'>
+          {mensaje} <br />
+          {imcMensaje}
+        </h2>
+
+        <button onClick={() => onReset()} type='reset' >
+          Limpiar
+        </button>
+
     <>
       <div className="flex items-center justify-center h-screen">
         <img src='https://www.consumer.es/app/uploads/2022/02/cintura-obesidad-imc.jpg' className="w-full h-[80vh] mt-[8%]" />
@@ -15,8 +112,11 @@ export default function Calculadora() {
             debes tener una cuenta <Link to="" className="text-indigo-800">PREMIUM</Link>
           </h1>
         </div>
+
       </div>
-    </>
-  )
+    </div>
+  );
 }
+
+export default Calculadora;
 
