667f2ed9e7b08d2159f96841a66416c4c0189d82
Martin Angulo <martinangulo1194@gmail.com>
2022-09-24 21:58:21 -0500

    front/ fix avatar img and visitor 3

--- client/src/Components/HomeVisitor/visitor_3/Visitor3.tsx
+++ client/src/Components/HomeVisitor/visitor_3/Visitor3.tsx
@@ -6,7 +6,7 @@ export default function Visitor3() {
   return (
     <div className={styles.container}>
       <div className={styles.text}>
-        <h1 className={styles.tittle}>¿Tienes entre 20 y 50 años?</h1>
+        <h1 className={styles.tittle}>¿Tienes mas de 20?</h1>
         <p className={styles.p}>Entonces esta app esta hecha a tu medida</p>
         <Link to={'/auth/sing-up'}>Empieza a cambiar tu vida ya!</Link>
       </div>
