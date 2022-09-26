21eff1d6710313164475ce846eb010eff8a9958e
RafaelBetancourt <rafael18282@gmail.com>
2022-09-25 21:51:47 -0500

    ejercicios random

--- client/src/Components/HomeRegister/News/CardNews.tsx
+++ client/src/Components/HomeRegister/News/CardNews.tsx
@@ -10,7 +10,7 @@ const CardNews = ({ title, description, author, date, image, tags, id }: newsInt
 
     return (
         <div className="my-10 mx-3">
-            <section className="flex flex-col justify-center antialiased bg-gray-200 text-gray-900 min-h-full rounded-2xl">
+            <section className="flex flex-col justify-center antialiased bg-gray-200 text-gray-900 min-h-full ">
                 <div className="max-w-6xl mx-auto p-4 sm:px-6 h-full">
                     {/* Blog post */}
                     <article className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
