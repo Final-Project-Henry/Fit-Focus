e503627ffb560f4d3f56599c5ac9256e523ae6eb
Rafael Betancourt <Rafael18282@gmail.com>
2022-09-27 12:36:04 -0500

    Langs (#239)
    * package update
    
    * Add feature to Clean button
    
    * json ES home visitor, algunos detalles gramaticals

--- client/src/Components/HomeVisitor/visitor_2/Visitor2.tsx
+++ client/src/Components/HomeVisitor/visitor_2/Visitor2.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from './Visitor2.module.css';
-const laptop =require('../../assets/Fotos y Videos HomeVisitor/laptop page.mp4');
+const laptop = require('../../assets/Fotos y Videos HomeVisitor/laptop page.mp4');
 
 export default function Visitor2() {
   return (
     <div className={styles.container}>
-        <video className={styles.animation} src={laptop} autoPlay loop muted></video>
+      <video className={styles.animation} src={laptop} autoPlay loop muted></video>
       <div className={styles.container_2}>
-        <h1 className={styles.tittle}>Esta app:</h1>
+        <h1 className={styles.tittle}>Esta aplicación:</h1>
         <div className={styles.text}>
-          <p>Ofrece una gran variedad de ejercicios</p>
-          <p>para realizarlos en casa, dependiendo</p>
-          <p>la parte del cuerpo que se desee trabajar.</p>
-          <p>Tambien nos brinda una calculadora</p>
-          <p>para conocer el indice de masa corporal</p>
-          <p>y asi tener un mayor conocimiento de</p>
-          <p>nuestra salud.</p>
+
+          <p>
+            Ofrece una gran variedad de ejercicios
+            para realizarlos en casa, dependiendo
+            la parte del cuerpo que se desee trabajar.
+            Tambien nos brinda una calculadora
+            para conocer el indice de masa corporal
+            y asi tener un mayor conocimiento de
+            nuestra salud.
+          </p>
         </div>
       </div>
     </div>
--- client/src/Components/HomeVisitor/visitor_5/Visitor5.tsx
+++ client/src/Components/HomeVisitor/visitor_5/Visitor5.tsx
@@ -5,9 +5,9 @@ export default function Visitor5() {
   return (
     <div className={styles.container}>
         <div className={styles.container_2}>
-            <p>“La edad no es una barrera.</p>
-            <p>Es una limitación que pones</p>
-            <p>en tu mente.”</p>
+            <p>“La edad no es una barrera.
+            Es una limitación que pones
+            en tu mente.”</p>
         </div>
     </div>
   )
