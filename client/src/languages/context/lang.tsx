5f8f64d25981bcec5bc7efa0c9f5fee74bd90c4b
David Cicconi <57296669+davidcicconi94@users.noreply.github.com>
2022-09-27 15:05:59 -0300

    add context lang (#241)

--- client/src/languages/context/lang.tsx
+++ client/src/languages/context/lang.tsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, {
+  useReducer,
+  useContext,
+  FC,
+  ReactNode,
+  createContext,
+} from "react";
+import es from "../es.json";
+import en from "../en.json";
 
-const lang = () => {
-  return <div></div>;
+// Los enum en TS son un conjunto de constantes con nombre
+enum LangActionType {
+  SET_LANGUAGE = "SET_LANGUAGE",
+}
+
+interface LangState {
+  language: string;
+}
+
+interface LangStateProps {
+  children: ReactNode;
+}
+
+interface setLanguageAction {
+  type: typeof LangActionType.SET_LANGUAGE;
+  payload: string;
+}
+
+interface ContextProps {
+  state: LangState;
+  dispatch: {
+    setLanguage: (lang: string) => void;
+    traslate: (key: string) => string;
+  };
+}
+
+const langReducer = (
+  state: LangState,
+  action: setLanguageAction
+): LangState => {
+  switch (action.type) {
+    case LangActionType.SET_LANGUAGE:
+      return {
+        language: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
 
-export default lang;
+const localStorageLang = localStorage.getItem("language");
+const initialState = {
+  language: localStorageLang ? localStorageLang : "EN",
+};
+
+export const LangContext = createContext({} as ContextProps);
+
+const LangState: FC<LangStateProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(langReducer, initialState);
+  const setLanguage = (lang: string) => {
+    localStorage.setItem("language", lang);
+    dispatch({
+      type: LangActionType.SET_LANGUAGE,
+      payload: lang,
+    });
+  };
+  const traslate = (key: string): string => {
+    const { language } = state;
+    let langData: { [key: string]: any } = {};
+
+    if (language === "EN") {
+      langData = en;
+    } else if (language === "ES") {
+      langData = es;
+    }
+    return langData[key];
+  };
+  return (
+    <LangContext.Provider
+      value={{ state, dispatch: { setLanguage, traslate } }}
+    >
+      {children}
+    </LangContext.Provider>
+  );
+};
+
+export default LangState;
