ccf40faae83fbe556c047fa1eb44d98ef5acd01d
Rafael Betancourt <Rafael18282@gmail.com>
2022-09-28 13:56:04 -0500

    Add contacts (#255)
    * package update
    
    * Add feature to Clean button
    
    * footer visitor arreglado y margenes de banner
    
    * Contactos agregados, componente Nosotros y ruta.

--- client/src/Components/footer/Footer.tsx
+++ client/src/Components/footer/Footer.tsx
@@ -105,7 +105,7 @@ const Footer = () => {
             
             <li className="mb-4">
                 <a
-                  href="https://www.soyhenry.com/"
+                  href="/nosotros"
                   target="_blank"
                   className="hover:underline"
                 >
@@ -131,7 +131,7 @@ const Footer = () => {
             </h2>
             <ul className="text-gray-400">
               <li className="mb-4">
-                <a href="#" className="hover:underline ">
+                <a href="https://github.com/" className="hover:underline ">
                   Github
                 </a>
               </li>
@@ -191,7 +191,7 @@ const Footer = () => {
             <span className="sr-only">Facebook page</span>
           </a>
           <a
-            href="#"
+            href="https://www.instagram.com/fitfocushenry/"
             target="_blank"
             className="text-gray-500 hover:text-white"
           >
@@ -225,7 +225,7 @@ const Footer = () => {
             <span className="sr-only">Twitter page</span>
           </a>
           <a
-            href="#"
+            href="https://github.com/"
             target="_blank"
             className="text-gray-500 hover:text-white"
           >
