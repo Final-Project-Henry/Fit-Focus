884cf2e68fdcb19582f260b2de7604974433ef5b
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-09-29 13:26:54 -0400

    Cambio de nombre

--- client/src/Components/Profile/ProfileDetails.tsx
+++ client/src/Components/Profile/ProfileDetails.tsx
@@ -159,8 +159,8 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                     <div className="relative flex flex-col h-full min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
                         <div className="p-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
                             <div className="flex flex-wrap -mx-3">
-                                <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none">
-                                    <h6 className="mb-0 font-bold text-xl">{editable.info ? "Editar perfil" : "Detalles del perfil"}</h6>
+                                <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-auto md:flex-none">
+                                    <h6 className="mb-0 font-bold text-xl">{editable.info ? "Editar usuario" : "Detalles del usuario"}</h6>
                                 </div>
                                 <div className="w-full max-w-full px-3 text-right shrink-0 md:w-4/12 md:flex-none">
                                     <a data-target="tooltip_trigger" data-placement="top">
@@ -268,7 +268,7 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                         <div className="flex-auto p-4">
                             <hr className="h-px my-2 bg-transparent bg-gradient-horizontal-light" />
                             <ul className="grid grid-flow-row grid-cols-2 pl-0 mb-0 rounded-lg">
-                                <li className="relative block px-4 py-2 pl-0 leading-normal bg-white border-0 border-t-0 text-size-base text-inherit">
+                                <li className="relative block px-4 pb-2 pl-0 leading-normal bg-white border-0 border-t-0 text-size-base text-inherit">
                                     <strong className="text-slate-700 mr-0.5">
                                         {editable.status ? "Editar peso:" : "Peso:"}
                                     </strong>
@@ -276,7 +276,7 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                                     {statusUser.weight !== undefined && <span className={`${editable.status && "ml-1 text-gray-400 duration-500"} ml-0`}>kg</span>}
                                 </li>
                                 {/*  */}
-                                <li className="relative block px-4 py-2 pl-0 leading-normal bg-white border-0 border-t-0 text-size-base text-inherit">
+                                <li className="relative block px-4 pb-2 pl-0 leading-normal bg-white border-0 border-t-0 text-size-base text-inherit">
                                     <strong className="text-slate-700 mr-0.5">
                                         {editable.status ? "Editar edad:" : "Edad:"}
                                     </strong>
@@ -292,7 +292,7 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                                     {statusUser.weight !== undefined && <span className={`${editable.status && "ml-1 text-gray-400 duration-500"} ml-0`}>cm</span>}
                                 </li>
                                 {/*  */}
-                                <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
+                                <li className="relative block px-4 py-2 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
                                     <strong className="text-slate-700 mr-0.5">
                                         {editable.status ? "Editar equipo:" : "Equipo:"}
                                     </strong>
@@ -306,7 +306,7 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                                     }
                                 </li>
                                 {/*  */}
-                                <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
+                                <li className="relative block px-4 py-2 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
                                     <strong className="text-slate-700 mr-0.5">
                                         {editable.status ? "Editar Genero:" : "Genero:"}
                                     </strong>
@@ -320,7 +320,7 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                                     }
                                 </li>
                                 {/*  */}
-                                <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
+                                <li className="relative block px-4 pt-2 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
                                     <strong className="text-slate-700 mr-0.5">
                                         {editable.status ? "Editar objetivo:" : "Objetivo:"}
                                     </strong>
@@ -334,7 +334,7 @@ const ProfileDetails = ({ name, email, plan, age, equipment, experience, genre, 
                                     }
                                 </li>
                                 {/*  */}
-                                <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
+                                <li className="relative block px-4 pb-2 pl-0 leading-normal bg-white border-0 rounded-t-lg text-size-base text-inherit">
                                     <strong className="text-slate-700 mr-0.5">
                                         {editable.status ? "Editar experiencia:" : "Experiencia:"}
                                     </strong>
