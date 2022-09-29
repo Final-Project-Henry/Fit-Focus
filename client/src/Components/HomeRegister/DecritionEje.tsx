50ac4b5692016134bcc59ed43a6ce556812ad14e
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2022-09-28 21:33:34 -0300

    fix decription

--- client/src/Components/HomeRegister/DecritionEje.tsx
+++ client/src/Components/HomeRegister/DecritionEje.tsx
@@ -244,7 +244,7 @@ export default function DecriptionEjer() {
                    </div>
                    </div>
                    <span className ="p-2 flex text-red-700 text-lg justify-end w-full">
-                   {user.email!==email&&<span className ="cursor-pointer  hover:after:content-['Banear'] after:p-2 after:absolute after:text-gray-500 duration-150">
+                   {user?.email!==email&&<span className ="cursor-pointer  hover:after:content-['Banear'] after:p-2 after:absolute after:text-gray-500 duration-150">
                       <FaBan/>
                     </span>
                     }
@@ -259,7 +259,7 @@ export default function DecriptionEjer() {
           <div className="flex">
             <img className="rounded-full w-[10%] m-2" src="https://randomuser.me/api/portraits/men/97.jpg" />
              <div className="flex flex-col">
-               <p className="m-2">{user.email}</p>
+               <p className="m-2">{user?.email}</p>
                 <div className="flex">
                 <svg aria-hidden="true" className={`w-5 h-5  ${rewind.cont>=1?"text-yellow-400":"text-gray-500"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                 <svg  aria-hidden="true" className={`w-5 h-5  ${rewind.cont>=2?"text-yellow-400":"text-gray-500"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
