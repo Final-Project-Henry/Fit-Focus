72ce14ac4f8506efa6ac673c2b24148b899e8967
Rafael Betancourt <Rafael18282@gmail.com>
2022-09-29 10:16:23 -0500

    Terminosagregados (#259)
    * package update
    
    * Add feature to Clean button
    
    * footer visitor arreglado y margenes de banner
    
    * terminos agregados

--- client/src/Components/terminosycondiciones/TerminosYCondiciones.tsx
+++ client/src/Components/terminosycondiciones/TerminosYCondiciones.tsx
@@ -15,6 +15,24 @@ function TerminosYCondiciones() {
                     1.1 El acceso y uso por usted de los Sitios y los servicios, el software y los productos de <b>FIT FOCUS</b> a través de los Sitios, lo cual incluirá las plataformas web está sujeto a los términos y las condiciones contenidos en este documento, así como en la Política de privacidad, la Política de listado de ejercicios y cualquier otra normativa y política de los Sitios que pueda publicar <b>FIT FOCUS</b> puntualmente. En lo sucesivo, el presente documento y dichas otras normativas y políticas de los Sitios se denominan, conjuntamente, los "Términos". Al acceder a y utilizar los Sitios y Servicios, usted se compromete a aceptar y someterse (BAJO SU PROPIA RESPONSABILIDAD) a estos Términos de uso. Por favor, no acceda o utilice los Servicios o los Sitios si no acepta todos los Términos.
                     <br />
                     <br />
+                    En <b>FIT FOCUS</b> queremos mantener un entorno respetuoso para todos y, por eso, debes seguir estas normas de conducta basicas.
+                    <br/>
+                    <ul>
+                        <li>
+                            Respetar los derechos de los demás, incluidos los de privacidad y propiedad intelectual.
+                        </li>
+                        <li>
+                            No abusar o causar daños a otras personas ni a uno mismo (ni coaccionar o fomentar tales conductas); por ejemplo, a través del engaño, la estafa, la suplantacion de identidad ilegal, la difamación, el acoso, la intimdación o el acechamiento.
+                        </li>
+                        <li>
+                            No hacer uso inadecuado de los servicios ni dañarlos, interrumpirlos o interferir con ellos; por ejemplo, usandolos o accediendo a ellos de forma fraudulenta o engañosa, introduciendo software malicioso, enviando spam, realizando ataques de pirateria o eludiendo nuestros sistemas o medidas de protección.
+                        </li>
+                        <li>
+                            De no cumplir con las normas mencionadas previamente y incurrir en alguna de esas faltas, <b>FIT FOCUS</b> se reserva el derecho de suspender o cancelar el acceso del usuario a los servicios o de eliminar la cuenta.  
+                        </li>
+                    </ul>
+                    <br />
+                    <br />
                     <b>FIT FOCUS</b> está compuesto por un grupo de desarrolladores web, ninguno del equipo es nutricionista ni medico, <b>no nos hacemos responsables por el uso inadecuado de la aplicación</b>, si tiene algun antecedente medico grave o alguna condicion en su salud, <b>POR FAVOR CONSULTE A SU MEDICO PRIMERO.</b> 
                     <br />
                     <br />
