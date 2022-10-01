91d496f35fbb2ba64ccbc0996abf85eb989c4e46
David Cicconi <57296669+davidcicconi94@users.noreply.github.com>
2022-10-01 13:57:05 -0300

    add stylessidebar (#282)
    * add stylessidebar
    
    * fix sidebar

--- client/src/Components/admin/additional/routes.ts
+++ client/src/Components/admin/additional/routes.ts
@@ -1,39 +1,38 @@
-import Home from '../imgs/home.png';
-import Users from '../imgs/userss.png';
-import Exercises from '../imgs/exer.png';
-import Comments from '../imgs/comments.png';
-import News from '../imgs/news.png';
-import Questions from '../imgs/qya.png';
+import Home from "../imgs/home.png";
+import Users from "../imgs/userss.png";
+import Exercises from "../imgs/exer.png";
+import Comments from "../imgs/comments.png";
+import News from "../imgs/news.png";
+import Questions from "../imgs/qya.png";
 
 export default [
-    {
-        img: Home,
-        url: '',
-        tittle: "Home",
-    },
-    {
-        img: Users,
-        url: 'users',
-        tittle: "Users",
-    },
-    {
-        img: Exercises,
-        url: 'exercises',
-        tittle: "Exercises",
-    },
-    {
-        img: Comments,
-        url: 'comments',
-        tittle: "Comments",
-    },
-    {
-        img: News,
-        url: 'news',
-        tittle: "News",
-    },
-    {
-        img: Questions,
-        url: 'questions',
-        tittle: "Questions",
-    },
+  {
+    img: Home,
+    url: "",
+    tittle: "Home",
+  },
+  {
+    img: Users,
+    url: "users",
+    tittle: "Users",
+    gap: true,
+  },
+
+  {
+    img: Exercises,
+    url: "exercises",
+    tittle: "Exercises",
+  },
+  {
+    img: Comments,
+    url: "comments",
+    tittle: "Comments",
+    gap: true,
+  },
+
+  {
+    img: Questions,
+    url: "questions",
+    tittle: "Questions",
+  },
 ];
