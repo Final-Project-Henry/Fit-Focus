b9cf72c3655e91a6b72c57ae5e93829b751d6f20
David <david.cicconi94@gmail.com>
2022-10-01 14:34:21 -0300

    change colors

--- client/src/Components/admin/comments/Card.tsx
+++ client/src/Components/admin/comments/Card.tsx
@@ -1,28 +1,43 @@
-import React from 'react';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import CommentDetail from "./CommentDetail";
 
 interface feedback {
-  email: string,
-  comment: string,
-  rating: number | string
+  email: string;
+  comment: string;
+  rating: number | string;
 }
 
 interface datos {
-  name: string,
-  feedback: Array<feedback>
+  name: string;
+  feedback: Array<feedback>;
 }
 
 export default function Card(props: { datos: datos }) {
   return (
-    <div style={{ backgroundColor: "gray", margin: "20px", paddingBottom: "5px", display: "flex", flexDirection:"column"}}>
-      <h1 style={{color:"white", fontSize:"1.2rem", padding:"5px", marginLeft:"5px", fontWeight:"500"}}>{props.datos.name}</h1>
-      <div style={{display:"flex", flexWrap:"wrap"}}>
-        {
-          props.datos.feedback.map((e: feedback) => (
-            <CommentDetail feedback={e} />
-          ))
-        }
+    <div
+      className="bg-amber-200"
+      style={{
+        margin: "20px",
+        paddingBottom: "5px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "1.2rem",
+          padding: "5px",
+          marginLeft: "5px",
+          fontWeight: "500",
+        }}
+      >
+        {props.datos.name}
+      </h1>
+      <div style={{ display: "flex", flexWrap: "wrap", padding: "10px" }}>
+        {props.datos.feedback.map((e: feedback) => (
+          <CommentDetail feedback={e} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
