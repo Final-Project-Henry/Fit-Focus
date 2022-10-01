cae0cef49dc03678c2a172f15577143484aee818
David <david.cicconi94@gmail.com>
2022-10-01 13:36:34 -0300

    add stylessidebar

--- client/src/Components/admin/additional/routes.ts
+++ client/src/Components/admin/additional/routes.ts
@@ -1,39 +1,35 @@
-import Home from '../imgs/home.png';
-import Users from '../imgs/userss.png';
-import Exercises from '../imgs/exer.png';
-import Comments from '../imgs/comments.png';
-import News from '../imgs/news.png';
-import Questions from '../imgs/qya.png';
+import Home from "../imgs/home.png";
+import Users from "../imgs/userss.png";
+import Exercises from "../imgs/exer.png";
+import Comments from "../imgs/comments.png";
+import News from "../imgs/news.png";
+import Questions from "../imgs/qya.png";
 
 export default [
-    {
-        img: Home,
-        url: '',
-        tittle: "Home",
-    },
-    {
-        img: Users,
-        url: 'users',
-        tittle: "Users",
-    },
-    {
-        img: Exercises,
-        url: 'exercises',
-        tittle: "Exercises",
-    },
-    {
-        img: Comments,
-        url: 'comments',
-        tittle: "Comments",
-    },
-    {
-        img: News,
-        url: 'news',
-        tittle: "News",
-    },
-    {
-        img: Questions,
-        url: 'questions',
-        tittle: "Questions",
-    },
+  {
+    img: Home,
+    url: "",
+    tittle: "Home",
+  },
+  {
+    img: Users,
+    url: "users",
+    tittle: "Users",
+  },
+  {
+    img: Exercises,
+    url: "exercises",
+    tittle: "Exercises",
+  },
+  {
+    img: Comments,
+    url: "comments",
+    tittle: "Comments",
+  },
+
+  {
+    img: Questions,
+    url: "questions",
+    tittle: "Questions",
+  },
 ];
--- client/src/Components/admin/home/DataCard.tsx
+++ client/src/Components/admin/home/DataCard.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React from "react";
 
 interface prop {
-    img: any,
-    tittle: string,
-    value: string | number | undefined,
+  img: any;
+  tittle: string;
+  value: string | number | undefined;
 }
 
-export default function DataCard(props:{data:prop}) {
-    return (
-        <div style={{display:"flex", backgroundColor:"gray"}}>
-            <img src={props.data.img} width="100px"/>
-            <div style={{display:"flex", flexDirection:"column", width:"15vw", alignItems:"center", justifyContent:"center", gap:"10px"}}>
-                <h1>{props.data.tittle}</h1>
-                <p>{props.data.value}</p>
-            </div>
-        </div>
-    )
+export default function DataCard(props: { data: prop }) {
+  return (
+    <div className="flex bg-gray-200 p-4">
+      <img src={props.data.img} width="85px" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "15vw",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "10px",
+        }}
+      >
+        <h1>
+          <b>{props.data.tittle}</b>
+        </h1>
+        <p className="text-md">{props.data.value}</p>
+      </div>
+    </div>
+  );
 }
