96979ed9bd067d37685486244f4f58d56ff36b49
Caterina Aracil <caterinaaracil@gmail.com>
2022-10-02 00:01:04 -0300

    Update nodemailer.js

--- api/additional/nodemailer.js
+++ api/additional/nodemailer.js
@@ -16,7 +16,18 @@ module.exports = {
             from: 'fitfocus43@gmail.com',
             to: email,
             subject: '¡Bienvenido a Fit Focus!',
-            html: "<b> ¡Su usuario ha sido creado con éxito! Nos alegra que te hayas registrado. Nos alegra que estes aqui. Fit Focus tiene una comunidad enorme y apasionada, lista para ver y celebrar todas las cosas que te gustan, y hemos apartado un asiento solo para ti. Al crear una cuenta, aceptas los Términos de servicio y la Política de privacidad de Fit Focus. </b>",
+            html: `
+            <b>¡Su usuario ha sido creado con éxito! </b>
+            <p> Nos alegra que estes aqui. Fit Focus tiene una comunidad enorme y apasionada, lista para ver y celebrar todas las cosas que te gustan, y hemos apartado un asiento solo para ti. </p> 
+            <b> Al crear una cuenta, aceptas los Términos y la Política de privacidad de Fit Focus.   </b>
+             
+              <br/>
+              <br/>
+                
+                <img width=50% src="https://cdn.cienradios.com/wp-content/uploads/sites/3/2020/04/entrenar.jpg"></img>
+            `
+            ,
+            
         }
     },
     mailDelete: (email) => {
@@ -24,7 +35,19 @@ module.exports = {
             from: 'fitfocus43@gmail.com',
             to: email,
             subject: '¡Regresa cuando quieras!',
-            html: "<b> Su usuario ha sido desactivado temporalmente. sin embargo sientese libre de regresar cuando guste. </b>",
+            html: `
+            <b>Tu cuenta ha sido desactivada exitosamente. </b>
+            <p> ¡Regrasa cuendo quieras! Te estaremos esperando.</p>
+            <p>Esta desactivación implica que: </p>
+            <p> Tu perfil y todo lo asociado a el desaparecerá de Fit Focus inmediatamente aunque quedará guardado en nuestro servidor por si algún día decides volver a activarla.
+            Para reactivarla, solo tienes que acceder con tu correo electrónico y contraseña de inicio de sesión. Es posible que la reactivación de la cuenta tarde hasta 24 horas.  </p>
+            
+              <br/>
+              <br/>
+                
+                <img width=50% src="https://i0.wp.com/brunoramos.es/wp-content/uploads/2020/05/cuenta-adsense-inhabilitada.png?resize=605%2C320&ssl=1"></img>
+            `
+            ,
         }
  }, 
     mailNewPassword: (email , link) => {
@@ -33,14 +56,15 @@ module.exports = {
             to: email,
             subject: 'Nueva contraseña',
             html: `
-            <p>para cambiar tu contraseña rederijete al link de abajo y crea una nueva contraseña</p>
-            <b> Este link se vence en 10 minutos. </b>
+            <p>Opss! ¿Olvidaste tu contraseña? </p>
+            <p> No hay problema, para cambiarla rederijete al siguiente enlace y crea una nueva. </p>
+            <b> Este vence en 10 minutos. </b>
             <a href=${link}>${link}</a>
     
                 <br/>
                 <br/>
-                <p>Fit Focus te recomienda ejercisios de buena calidad para veneficiar tu memoria  </p>
-                <img width=50% src="https://www.entrenamiento.com/wp-content/uploads/2016/06/Ejercicio-mancuerna.jpg"></img>
+                
+                <img width=50% src="https://img.freepik.com/vector-gratis/ilustracion-concepto-olvide-contrasena_114360-1010.jpg?size=338&ext=jpg></img>
             `
             ,
         }
