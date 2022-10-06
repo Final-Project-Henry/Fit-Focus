be225841262075ef269923f6aeee3e7f5190d09f
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-10-05 23:51:11 -0400

    Input edad (#360)
    * • Agregar genero 'otro' en el form
    • Warnings en homeVisitor
    • Arregle el boton para scrollear al inicio que no aparecia en la primera montada de la pagina
    • Eliminacion de console.log()
    • Warnings en "nosotros" y "contactanos"
    • Warning de html en "Terminos y condiciones" y pequeñás mejoras en el texto
    
    * Edad form
    
    * input
    
    * input edad

--- client/src/Components/form_rutinas/From_rutina.tsx
+++ client/src/Components/form_rutinas/From_rutina.tsx
@@ -237,7 +237,14 @@ export default function Form_rutinas(props: { function: { (): void } }) {
                 <br />
               </div>
               <div className="relative z-0 mb-6 w-[100%] group mt-5">
-               
+                <input
+                  type="number"
+                  name="age"
+                  value={form_data?.age}
+                  onChange={handleChange}
+                  className="block py-1.2 px-2 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  peer"
+                  placeholder=" "
+                />
                 <label
                   htmlFor="Año"
                   className=" peer-focus:font-medium absolute text-sm text-gray-700 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
