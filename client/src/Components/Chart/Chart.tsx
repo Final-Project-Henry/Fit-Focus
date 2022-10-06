c46c3babd98aa1aaf270bf48a8fad72a2d1b48cf
Adrian Acurero <105818616+Adr-AA@users.noreply.github.com>
2022-10-06 05:03:49 -0400

    Graficas
    Co-authored-by: David Cicconi <davidcicconi94@users.noreply.github.com>

--- client/src/Components/Chart/Chart.tsx
+++ client/src/Components/Chart/Chart.tsx
@@ -9,7 +9,7 @@ import {
     Title,
     Tooltip,
     Legend,
-    Filler
+    Filler,
 } from "chart.js"
 import {Chart} from "react-chartjs-2"
 
@@ -21,7 +21,7 @@ ChartJS.register(
     Title,
     Tooltip,
     Legend,
-    Filler
+    Filler,
 )
 
 interface props {
